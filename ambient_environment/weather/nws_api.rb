c52c579e702f3329c23f699030e437b6770771d5
Jonathan Zingale <jonzingale@gmail.com>
2015-12-08 18:15:50 -0700

    cleaned up nws_api

--- ambient_environment/weather/nws_api.rb
+++ ambient_environment/weather/nws_api.rb
@@ -1,32 +1,26 @@
-
-#!/usr/bin/env ruby 
 	require 'byebug'
-
-	TOKEN = 'wvLwKMPNpxlWzKtegSBvuVjygWhgJjnu'
-	CITY_DATA = [['santa fe','87501',[441, 372],'KSAF']].freeze
-	BASE_URL = 'http://www.ncdc.noaa.gov/cdo-web/api/v2/stations'.freeze
 
 	class Place
 		require 'mechanize'
 		require 'xmlsimple'
 
-		attr_reader :name, :zipcode, :coords, :geocoords, :agent, :source_id
+		BASE_URL = 'http://w1.weather.gov/xml/current_obs/KSAF.xml'.freeze
+
+		attr_reader :geocoords, :agent
 		attr_accessor :temp, :humidity, :page, :pressure, :dewpoint
 
-		def initialize name, zipcode, coords, source_id
-			@name, @zipcode, @coords, @source_id = name, zipcode, coords, source_id
-
+		def initialize
 			@agent = Mechanize.new
 			agent.follow_meta_refresh = true # new data
 			agent.keep_alive = false # no time outs
-			@page = agent.get('http://w1.weather.gov/xml/current_obs/KSAF.xml')	
+			@page = agent.get(BASE_URL)
 			xml_data
 		end
 
 		def xml_data
       response = XmlSimple.xml_in(page.body.to_s)
 			# response.keys.each{|k| puts "#{k} = response['#{k}']" }
-byebug
+
       temp = response['temp_f'][0].to_f
       wind_mph = response['wind_mph'][0].to_f
       # location = response['location'][0].to_f
@@ -34,7 +28,8 @@ byebug
       latitude = response['latitude'][0].to_f
       longitude = response['longitude'][0].to_f
 
-      windchill_f = response['windchill_f'][0].to_f
+      # protect against missing keys!
+      # windchill_f = response['windchill_f'][0].to_f
       station_id = response['station_id'][0].to_f
       dewpoint_f = response['dewpoint_f'][0].to_f
 			pressure_in = response['pressure_in'][0].to_f
@@ -49,20 +44,7 @@ byebug
 			@dewpoint = dewpoint_f
 			@geocoords = [longitude, latitude]
 		end
-
 	end
 
-	@cities = CITY_DATA.map{|data| Place.new(*data) }
-	@cities.each{|city| city.xml_data}
-
-byebug ; 4
-
-	# working curl
-	# curl 'http://www.ncdc.noaa.gov/cdo-web/api/v2/stations' -H 'token:wvLwKMPNpxlWzKtegSBvuVjygWhgJjnu'
-
-	# agent = Mechanize.new
-	# agent.pre_connect_hooks << lambda do |agent, request|
-	#    request['token'] = 'wvLwKMPNpxlWzKtegSBvuVjygWhgJjnu'
-	#  end
-# http://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes/{id}
-  # response = agent.get('http://www.ncdc.noaa.gov/cdo-web/api/v2/datatypes/KSAF')
+	# uncomment when testing.
+	# santa_fe = Place.new
